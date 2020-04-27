4574d2c706545a72cbb96a5828ec2796b2c22ddb
arnab-shanta-anu <arnabshantaanu@gmail.com>
2020-04-28 02:09:29 +0600

    a refined version with download progress

--- download-script.py
+++ download-script.py
@@ -2,14 +2,18 @@
 import os
 import json
 import requests
-from git import Repo
+import git
 
 response = requests.get("https://api.github.com/users/arnab-shanta-anu/repos")
 repos = json.loads(response.text)
 
+class Progress(git.remote.RemoteProgress):
+	def update(self, op_code, cur_count, max_count=None, message=''):
+		print (self._cur_line)
+
 for repo in repos:
 	try:
-		Repo.clone_from(repo["clone_url"],repo["name"])
+		git.Repo.clone_from(repo["clone_url"], repo["name"], progress=Progress())
 		print('cloning ' +repo["name"]+ ' into '+ os.getcwd()+'/'+repo["name"])
 	except KeyError:
 		print('some error')
