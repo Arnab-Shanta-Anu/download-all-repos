3de53b94f9e640f4c9625864eaa08fc8393885af
arnab-shanta-anu <arnabshantaanu@gmail.com>
2020-05-02 14:07:52 +0600

    implementing switch case

--- download-script.py
+++ download-script.py
@@ -5,9 +5,19 @@ import json
 import requests
 import git
 
-username = input("Enter your github username: ")
-response = requests.get("https://api.github.com/users/"+username+"/repos")
+userName = input("Enter your github username: ")
+response = requests.get("https://api.github.com/users/"+userName+"/repos")
 repos = json.loads(response.text)
+
+if (len(repos) == 2 and repos["message"] == "Not Found"):
+    print("no user in that name found\nRun program again")
+    exit(1)
+
+saveLoc = input("enter directory name to save in[" + os.getcwd() + "]: ")
+
+if (saveLoc == "\n"):
+    saveLoc = os.getcwd()
+
 print("totoal repos: ", len(repos))
 i = 1
 for repo in repos:
@@ -15,14 +25,14 @@ for repo in repos:
     print(i, ") " + repo["name"]+"\t")
     i += 1
 
-op = input("options[1]:\n1.clone all\n2.clone seleceted\n3.ignore selected:")
+op = input("options[1]:\n1.clone all\n2.clone seleceted\n3.ignore selected: ")
 if (op == "\n"):
     op = 1
 
 
 class Progress(git.remote.RemoteProgress):
     def update(self, op_code, cur_count, max_count=None, message=''):
-        print(self._cur_line)
+        print(self._cur_line, end='')
         restart_line()
 
 
@@ -31,11 +41,24 @@ def restart_line():
     sys.stdout.flush()
 
 
-for repo in repos:
-    try:
-        git.Repo.clone_from(repo["clone_url"],
-                            repo["name"], progress=Progress())
-        print("cloning ", repo["name"])
+def download():
+    for repo in repos:
+        try:
+            git.Repo.clone_from(repo["clone_url"],
+                                repo["name"], progress=Progress())
+            print("cloning ", repo["name"])
 
-    except KeyError:
-        print('some error')
+        except git.exc.GitCommandError as e:
+            print('not an empty directory\ntrying to pull from master')
+
+
+def downloadAll():
+    download()
+
+
+def downloadSelected():
+    download()
+
+
+def ignoreSelected():
+    download()
