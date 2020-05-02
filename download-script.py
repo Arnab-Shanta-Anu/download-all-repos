a997cd8bb360c5f5fd3fc4460a9b3a0ab8ed2fbd
arnab-shanta-anu <arnabshantaanu@gmail.com>
2020-05-02 11:32:27 +0600

    giving user option to put in user name and options

--- download-script.py
+++ download-script.py
@@ -1,22 +1,41 @@
 #!/bin/python3
 import os
+import sys
 import json
 import requests
 import git
 
-response = requests.get("https://api.github.com/users/arnab-shanta-anu/repos")
+username = input("Enter your github username: ")
+response = requests.get("https://api.github.com/users/"+username+"/repos")
 repos = json.loads(response.text)
 print("totoal repos: ", len(repos))
+i = 1
+for repo in repos:
+
+    print(i, ") " + repo["name"]+"\t")
+    i += 1
+
+op = input("options[1]:\n1.clone all\n2.clone seleceted\n3.ignore selected:")
+if (op == "\n"):
+    op = 1
+
 
 class Progress(git.remote.RemoteProgress):
-	def update(self, op_code, cur_count, max_count=None, message=''):
-		print (self._cur_line)
+    def update(self, op_code, cur_count, max_count=None, message=''):
+        print(self._cur_line)
+        restart_line()
+
+
+def restart_line():
+    sys.stdout.write('\r')
+    sys.stdout.flush()
+
 
 for repo in repos:
-	try:
-		git.Repo.clone_from(repo["clone_url"], repo["name"], progress=Progress())
-		print('cloning ' +repo["name"]+ ' into '+ os.getcwd()+'/'+repo["name"])
-	except KeyError:
-		print('some error')
+    try:
+        git.Repo.clone_from(repo["clone_url"],
+                            repo["name"], progress=Progress())
+        print("cloning ", repo["name"])
 
-
+    except KeyError:
+        print('some error')
