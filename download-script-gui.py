84ed2def02a98032880231bcf972dc8b0aabc1c5
arnab-shanta-anu <arnabshantaanu@gmail.com>
2020-05-08 15:24:43 +0600

    gui

---
+++ download-script-gui.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+root = Tk()
+root.title("download-script")
+root.geometry("600x400+300+300")  # widthxheight+top height+left distance
+
+userNameLbl = Label(root, text="enter your username")
+userNameLbl.grid(row=0, column=0)
+userNameEntry = Entry(root, padX=)
+userNameEntry.grid(row=0, column=1)
+
+root.mainloop()
