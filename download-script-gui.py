1ba27a64830cf824a5fbaf8cc7bae7043e8ad4cc
arnab-shanta-anu <arnabshantaanu@gmail.com>
2020-05-09 14:03:31 +0600

    rem padX entry

--- download-script-gui.py
+++ download-script-gui.py
@@ -6,7 +6,7 @@ root.geometry("600x400+300+300")  # widthxheight+top height+left distance
 
 userNameLbl = Label(root, text="enter your username")
 userNameLbl.grid(row=0, column=0)
-userNameEntry = Entry(root, padX=)
+userNameEntry = Entry(root)
 userNameEntry.grid(row=0, column=1)
 
 root.mainloop()
